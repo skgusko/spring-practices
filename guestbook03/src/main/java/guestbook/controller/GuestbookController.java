03556aba0238062d0bfbfcb2e5f0469b0fc6e989
Konahyeon <koo@inha.edu>
2024-12-27 17:48:39 +0900

    ApplicationContext 확인

--- guestbook03/src/main/java/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/guestbook/controller/GuestbookController.java
@@ -1,7 +1,9 @@
 package guestbook.controller;
 
+import java.util.Enumeration;
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import guestbook.repository.GuestbookRepository;
 import guestbook.vo.GuestbookVo;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class GuestbookController {
@@ -21,12 +25,38 @@ public class GuestbookController {
 	}
 	
 	@RequestMapping("/")
-	public String index(Model model) {
+	public String index(HttpServletRequest request, Model model) {
+		ServletContext sc = request.getServletContext();
+		Enumeration<String> e = sc.getAttributeNames();
+		
+		while (e.hasMoreElements()) {
+			String name = e.nextElement();
+			System.out.println(name);
+		}
+		
+		ApplicationContext ac1 = (ApplicationContext)sc.getAttribute("org.springframework.web.context.WebApplicationContext.ROOT");
+		ApplicationContext ac2 = (ApplicationContext)sc.getAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.spring");
+		GuestbookRepository repository = ac1.getBean(GuestbookRepository.class);
+		System.out.println("====== repository : " + repository);
+		
+		GuestbookController controller = ac2.getBean(GuestbookController.class);
+		System.out.println("====== controller : " + controller);
+		
+		System.out.println(ac1 == ac2);
+		
 		List<GuestbookVo> list = guestbookRepository.findAll();
 		model.addAttribute("list", list);
 		
 		return "index";
 	}
+	
+//	@RequestMapping("/")
+//	public String index(Model model) {
+//		List<GuestbookVo> list = guestbookRepository.findAll();
+//		model.addAttribute("list", list);
+//		
+//		return "index";
+//	}
 	
 	@RequestMapping("/add")
 	public String add(GuestbookVo guestbookVo) {
