f2af1b3f066795ffc89c2627c260ce6e7763b017
Konahyeon <koo@inha.edu>
2024-12-31 14:37:21 +0900

    Repository Refactoring #2

--- guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
+++ guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
@@ -4,66 +4,52 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import guestbook.repository.template.JdbcContext;
-import guestbook.repository.template.StatementStrategy;
 import guestbook.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
 	private JdbcContext jdbcContext;
-	private DataSource dataSource;
+	//private DataSource dataSource; // JdbcContext에서 이미 주입 받았기에 주입받을 필요 x
 	
-	public GuestbookRepository(JdbcContext jdbcContext, DataSource dataSource) {
+	public GuestbookRepository(JdbcContext jdbcContext) {
 		this.jdbcContext = jdbcContext;
-		this.dataSource = dataSource;
 	}
 	
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<GuestbookVo>();
-		
-		try (
-				Connection conn = dataSource.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc");
-				ResultSet rs = pstmt.executeQuery();
-		)
-		{
-			while(rs.next()) {
-				Long id = rs.getLong(1);
-				String name = rs.getString(2);
-				String contents = rs.getString(3);
-				String regDate = rs.getString(4);
-				
-				GuestbookVo vo = new GuestbookVo();
-				vo.setId(id);
-				vo.setName(name);
-				vo.setContents(contents);
-				vo.setRegDate(regDate);
-				
-				result.add(vo);
-			}
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		}
-		return result;
+		return jdbcContext.queryForList(
+			"select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc",
+			new RowMapper<GuestbookVo>() {
+	
+				@Override
+				public GuestbookVo mapRow(ResultSet rs, int rowNum) throws SQLException {
+					GuestbookVo vo = new GuestbookVo();
+					
+					vo.setId(rs.getLong(1));
+					vo.setName(rs.getString(2));
+					vo.setContents(rs.getString(3));
+					vo.setRegDate(rs.getString(4));
+					
+					return vo;
+				}
+			}); 
 	}
 
 	public int insert(GuestbookVo vo) {
 		return jdbcContext.executeUpdate(
-				"insert into guestbook values(null, ?, ?, ?, now())",
-				new Object[] {vo.getName(), vo.getPassword(), vo.getContents()});
+			"insert into guestbook values(null, ?, ?, ?, now())",
+			new Object[] {vo.getName(), vo.getPassword(), vo.getContents()});
 	}
 
 	public int deleteByIdAndPassword(Long id, String password) {
 		return jdbcContext.executeUpdate(
-				"delete from guestbook where id=? and password=?",
-				new Object[] {id, password});
+			"delete from guestbook where id=? and password=?",
+			new Object[] {id, password});
 		
 		
 //		int count = 0;
@@ -84,21 +70,21 @@ public class GuestbookRepository {
 //		return count;
 	}
 
-	public String findPasswordById(Long id) {
-		String result = "";
-		
-		try (
-				Connection conn = dataSource.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("select password from guestbook where id=?");
-				ResultSet rs = pstmt.executeQuery();
-		)
-		{
-			if(rs.next()) {
-				result = rs.getString(1);
-			}
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		}
-		return result;
-	}
+//	public String findPasswordById(Long id) {
+//		String result = "";
+//		
+//		try (
+//				Connection conn = dataSource.getConnection();
+//				PreparedStatement pstmt = conn.prepareStatement("select password from guestbook where id=?");
+//				ResultSet rs = pstmt.executeQuery();
+//		)
+//		{
+//			if(rs.next()) {
+//				result = rs.getString(1);
+//			}
+//		} catch (SQLException e) {
+//			System.out.println("error: " + e);
+//		}
+//		return result;
+//	}
 }
--- guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
+++ guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
@@ -2,9 +2,14 @@ package guestbook.repository.template;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.RowMapper;
 
 public class JdbcContext {
 	
@@ -13,6 +18,37 @@ public class JdbcContext {
 		this.dataSource = dataSource;
 	}
 	
+	public <E> List<E> queryForList(String sql, RowMapper<E> rowMapper) {
+		return exequeryForListUpdateWithStatementStrategy(new StatementStrategy() {
+
+			@Override
+			public PreparedStatement makeStatement(Connection connection) throws SQLException {
+				return connection.prepareStatement(sql);
+			}
+			
+		}, rowMapper);
+	}
+	
+	private <E> List<E> exequeryForListUpdateWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) {
+		List<E> result = new ArrayList<>();
+		
+		try (
+				Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = statementStrategy.makeStatement(conn);
+				ResultSet rs = pstmt.executeQuery();
+		) {
+			while (rs.next()) {
+				E e = rowMapper.mapRow(rs, rs.getRow()); //row 한 줄 씩 넣기 (findAll 메서드에서 바인딩될 객체 지정해둠) 
+				result.add(e);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		}
+		
+		return result;
+	}
+
 	// 사용자인 Client가 좀 더 깔끔하게 사용할 수 있도록 아래처럼 랩핑하여 구현 
 	public int executeUpdate(String sql, Object[] parameters) {
 		return executeUpdateWithStatementStrategy(new StatementStrategy() {
@@ -40,4 +76,19 @@ public class JdbcContext {
 		}
 		return count;
 	}
+
+	
+//	private int executeUpdateWithStatementStrategy(StatementStrategy statementStrategy) { //operation()
+//		int count = 0;
+//		
+//		try (
+//				Connection conn = dataSource.getConnection();
+//				PreparedStatement pstmt = statementStrategy.makeStatement(conn);
+//		) {
+//			count = pstmt.executeUpdate();
+//		} catch (SQLException e) {
+//			System.out.println("error: " + e);
+//		}
+//		return count;
+//	}
 }
