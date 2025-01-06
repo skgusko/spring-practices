76a12c1d867d6852d8b197499b68b5f61eb229fc
Konahyeon <koo@inha.edu>
2025-01-06 13:19:43 +0900

    interceptor

---
+++ hellospring/src/main/java/hellospring/interceptor/MyInterceptor.java
@@ -0,0 +1,31 @@
+package hellospring.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("MyInterceptor.preHandle() called");
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("MyInterceptor.postHandle() called");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("MyInterceptor.afterCompletion() called");
+	}
+
+}
